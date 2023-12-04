dfa64f64b1a47e5e2834f5e6de735078583ef857
corinnt <94402869+corinnt@users.noreply.github.com>
2023-12-03 21:09:24 -0500

    progress on adapting GC-MERGE

--- module4_gcn/gcn_model_classes.py
+++ module4_gcn/gcn_model_classes.py
@@ -10,137 +10,12 @@ from sklearn.metrics import roc_auc_score, f1_score, precision_recall_curve, auc
 from scipy.stats import pearsonr
 import pandas as pd
 
-import pytorch as nn
+import torch as nn
 import torch_geometric
 import torch.nn.functional as F
 import torch.nn as nn
 
-from sage_conv_cat_ import SAGEConvCat # TODO replace SAGEConvCat 
-
-
-class GCN_regression(nn.Module):
-
-    def __init__(self, num_feat, num_graph_conv_layers, graph_conv_layer_sizes, num_lin_layers, lin_hidden_sizes, num_classes):
-        '''
-        Defines regression model class
-
-        Parameters
-        ----------
-        num_feat [int]: Feature dimension (int)
-        num_graph_conv_layers [int]: Number of graph convolutional layers (1, 2, or 3)
-        graph_conv_layer_sizes [int]: Embedding size of graph convolutional layers 
-        num_lin_layers [int]: Number of linear layers (1, 2, or 3)
-        lin_hidden_sizes [int]: Embedding size of hidden linear layers
-        num_classes [int]: Size of predicted output tensor for batch size of N, 
-            i.e. N x num_classes(=1)
-
-        Returns
-        -------
-        None.
-
-        '''
-        
-        super(GCN_regression, self).__init__()
-        
-        self.num_graph_conv_layers = num_graph_conv_layers
-        self.num_lin_layers = num_lin_layers
-        self.dropout = 0.5
-    
-        if self.num_graph_conv_layers == 1:
-            self.conv1 = SAGEConvCat(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
-        elif self.num_graph_conv_layers == 2:
-            self.conv1 = SAGEConvCat(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
-            self.conv2 = SAGEConvCat(graph_conv_layer_sizes[1], graph_conv_layer_sizes[2])
-        elif self.num_graph_conv_layers == 3:
-            self.conv1 = SAGEConvCat(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
-            self.conv2 = SAGEConvCat(graph_conv_layer_sizes[1], graph_conv_layer_sizes[2])
-            self.conv3 = SAGEConvCat(graph_conv_layer_sizes[2], graph_conv_layer_sizes[3])
-        
-        if self.num_lin_layers == 1:
-            self.lin1 = nn.Linear(lin_hidden_sizes[0], lin_hidden_sizes[1])
-        elif self.num_lin_layers == 2:
-            self.lin1 = nn.Linear(lin_hidden_sizes[0], lin_hidden_sizes[1])
-            self.lin2 = nn.Linear(lin_hidden_sizes[1], lin_hidden_sizes[2])
-        elif self.num_lin_layers == 3:
-            self.lin1 = nn.Linear(lin_hidden_sizes[0], lin_hidden_sizes[1])
-            self.lin2 = nn.Linear(lin_hidden_sizes[1], lin_hidden_sizes[2])
-            self.lin3 = nn.Linear(lin_hidden_sizes[2], lin_hidden_sizes[3])
-        
-        self.loss_calc = nn.MSELoss()
-
-        
-    def forward(self, x, edge_index, train_status=False):
-        '''
-        Forward function
-        
-        Parameters
-        ----------
-        x [tensor]: Node features
-        edge_index [tensor]: Subgraph mask
-        train_status [bool]: optional, set to True for dropout
-
-        Returns
-        -------
-        scores [tensor]: Predicted expression levels
-
-        '''
-        ### Graph convolution module
-        if self.num_graph_conv_layers == 1:
-            h = self.conv1(x, edge_index)
-            h = torch.relu(h)
-        elif self.num_graph_conv_layers == 2:
-            h = self.conv1(x, edge_index)
-            h = torch.relu(h)
-            h = self.conv2(h, edge_index)
-            h = torch.relu(h)
-        elif self.num_graph_conv_layers == 3:
-            h = self.conv1(x, edge_index)
-            h = torch.relu(h)
-            h = self.conv2(h, edge_index)
-            h = torch.relu(h)
-            h = self.conv3(h, edge_index)
-            h = torch.relu(h)
-
-        h = F.dropout(h, p = self.dropout, training=train_status)
-
-        if self.num_lin_layers == 1:
-            scores = self.lin1(h)
-        elif self.num_lin_layers == 2:
-            scores = self.lin1(h)
-            scores = torch.relu(scores)
-            scores = self.lin2(scores)
-        elif self.num_lin_layers == 3:
-            scores = self.lin1(h)
-            scores = torch.relu(scores)
-            scores = self.lin2(scores)
-            scores = torch.relu(scores)
-            scores = self.lin3(scores)
-        
-        if len(scores.size()) > 1:
-            scores = scores.squeeze()
-            
-        return scores
-    
-    
-    def loss(self, scores, targets):
-        '''
-        Calculates mean squared error loss
-        
-        Parameters
-        ----------
-        scores [tensor]: Predicted scores from forward function
-        labels [tensor]: Target scores 
-
-        Returns
-        -------
-        mse [tensor]: Mean squared error loss
-
-        '''
-        
-        mse = self.loss_calc(scores, targets)
-
-        return mse
-    
+from torch_geometric.nn import Sequential, GCNConv
 
 class GCN_classification(nn.Module):
 
@@ -170,14 +45,14 @@ class GCN_classification(nn.Module):
         self.dropout_value = 0.5
 
         if self.num_graph_conv_layers == 1:
-            self.conv1 = SAGEConvCat(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
+            self.conv1 = GCNConv(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
         elif self.num_graph_conv_layers == 2:
-            self.conv1 = SAGEConvCat(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
-            self.conv2 = SAGEConvCat(graph_conv_layer_sizes[1], graph_conv_layer_sizes[2])
+            self.conv1 = GCNConv(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
+            self.conv2 = GCNConv(graph_conv_layer_sizes[1], graph_conv_layer_sizes[2])
         elif self.num_graph_conv_layers == 3:
-            self.conv1 = SAGEConvCat(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
-            self.conv2 = SAGEConvCat(graph_conv_layer_sizes[1], graph_conv_layer_sizes[2])
-            self.conv3 = SAGEConvCat(graph_conv_layer_sizes[2], graph_conv_layer_sizes[3])
+            self.conv1 = GCNConv(graph_conv_layer_sizes[0], graph_conv_layer_sizes[1])
+            self.conv2 = GCNConv(graph_conv_layer_sizes[1], graph_conv_layer_sizes[2])
+            self.conv3 = GCNConv(graph_conv_layer_sizes[2], graph_conv_layer_sizes[3])
         
         if self.num_lin_layers == 1:
             self.lin1 = nn.Linear(lin_hidden_sizes[0], lin_hidden_sizes[1])
@@ -212,19 +87,19 @@ class GCN_classification(nn.Module):
         ### Graph convolution module
         if self.num_graph_conv_layers == 1:
             h = self.conv1(x, edge_index)
-            h = torch.relu(h)
+            h = nn.relu(h)
         elif self.num_graph_conv_layers == 2:
             h = self.conv1(x, edge_index)
-            h = torch.relu(h)
+            h = nn.relu(h)
             h = self.conv2(h, edge_index)
-            h = torch.relu(h)
+            h = nn.relu(h)
         elif self.num_graph_conv_layers == 3:
             h = self.conv1(x, edge_index)
-            h = torch.relu(h)
+            h = nn.relu(h)
             h = self.conv2(h, edge_index)
-            h = torch.relu(h)
+            h = nn.relu(h)
             h = self.conv3(h, edge_index)
-            h = torch.relu(h)
+            h = nn.relu(h)
             
         h = F.dropout(h, p = self.dropout_value, training=train_status)
 
@@ -233,13 +108,13 @@ class GCN_classification(nn.Module):
             scores = self.lin1(h)
         elif self.num_lin_layers == 2:
             scores = self.lin1(h)
-            scores = torch.relu(scores)
+            scores = nn.relu(scores)
             scores = self.lin2(scores)
         elif self.num_lin_layers == 3:
             scores = self.lin1(h)
-            scores = torch.relu(scores)
+            scores = nn.relu(scores)
             scores = self.lin2(scores)
-            scores = torch.relu(scores)
+            scores = nn.relu(scores)
             scores = self.lin3(scores)
         
         return scores
@@ -282,6 +157,6 @@ class GCN_classification(nn.Module):
         
         softmax = self.torch_softmax(scores)
         
-        predicted = torch.argmax(softmax, 1)
+        predicted = nn.argmax(softmax, 1)
         
         return softmax, predicted
---
+++ module4_gcn/hic-process.py
@@ -0,0 +1,168 @@
+'''
+    This file contains the scripts that handle parsing the HiC files
+    Currently I am only supporting, '.hic format'
+'''
+
+from concurrent.futures import process
+import hicstraw
+import os
+import numpy as np
+from scipy.sparse import csr_matrix, coo_matrix, vstack, hstack
+from scipy import sparse
+
+
+import time
+from functools import partial
+import multiprocessing
+from multiprocessing import Process
+from src.utils import create_entire_path_directory, hic_file_paths, PARSED_HIC_FILES_DIRECTORY, hic_data_resolution, download_file
+
+
+
+# Main Multiprocessing switch for the HiC parser
+MULTIPROCESSING=False
+
+def process_chromosome(hic, output, resolution, chromosome, debug):
+    '''
+        This function handles the bulk of the work of extraction of Chromosome from
+        the HiC file and storing it in its dense 2D contact map form
+        @params: hic <hicstraw.HiCFile>, HiC file object as returned by the hicstraw utility
+        @params: output <os.path>, path where to store the output files
+        @params: resolution <int>, resolution to sample the HiC data at
+        @params: chromosome <hicstraw.chromosome>, hicstraw chromosome objects that contains its name and misc properties
+        @returns: None
+    '''
+    index = chromosome.index
+    length = chromosome.length
+    name = chromosome.name
+    output_path = os.path.join(
+        output, 
+        'resolution_{}'.format(resolution)
+    )
+    create_entire_path_directory(output_path)
+    
+    name = name.split('chr')[-1]
+
+    output_path = os.path.join(
+        output_path, 
+        'chr{}.npz'.format(name)
+    )
+
+    if name in ['Y','MT', 'M']:
+        return 
+
+    if os.path.exists(output_path):
+        if debug: print('Already parsed!')
+        return
+
+    if debug: print('Starting parsing Chromosome {}'.format(name))
+
+    try:
+        chromosome_matrix = hic.getMatrixZoomData(
+            chromosome.name, chromosome.name, 
+            'observed', 'KR', 'BP', resolution                                          
+        )
+    except:
+        try: 
+            chromosome_matrix = hic.getMatrixZoomData(
+                chromosome.name, chromosome.name, 
+                'observed', 'SCALE', 'BP', resolution                                          
+            )
+        except:
+            print('Chromosome {} doesnt contain any informative rows'.format(name))
+            return 
+
+    informative_indexes = np.array(chromosome_matrix.getNormVector(index))
+    informative_indexes = np.where(np.isnan(informative_indexes)^True)[0]
+
+    if len(informative_indexes) == 0:
+        print('Chromosome {} doesnt contain any informative rows'.format(name))
+        return
+
+    results = chromosome_matrix.getRecords(0, length, 0, length)
+    
+    # Bottleneck step
+    results = np.array([[(r.binX//resolution), (r.binY//resolution), r.counts] for r in results])
+
+    N = length//resolution
+    mat = csr_matrix((results[:, 2], (results[:, 0], results[:, 1])), shape=(N,N))
+    mat = csr_matrix.todense(mat)
+    mat = mat.T
+    mat = mat + np.tril(mat, -1).T
+
+    np.savez_compressed(output_path, hic=mat, compact=informative_indexes, size=length)
+    print('Saving Chromosome at path {}'.format(output_path))
+    return True
+
+    
+    
+def parse_hic_file(path_to_hic_file, output, resolution=10000, debug=False):
+    '''
+        This function provides a wrapper on all the methods that 
+        reads the .hic file and stores individual chromosomes in a 
+        dense matrix format at provided location
+        @params: path_to_hic_file <os.path>, path to where hic file is stored
+        @params: output_directory <os.path>, path where to store the generated chromsomes
+        @params: resolution <int>, resolution at which we sample the HiC contacts, defaults to 10000
+        @returns: None
+    '''
+    print('Parsing out intra-chromosomal contact matrices from {} file.'.format(path_to_hic_file))
+    # Read the hic file into memory
+    hic = hicstraw.HiCFile(path_to_hic_file)
+    
+    if resolution not in hic.getResolutions():
+        print('Resolution not supported by the provided .hic file, try a resolution from list {}'.format(
+            hic.getResolutions()
+        ))
+        exit(1)
+    
+    # Get all the available chromosomes
+    chromosomes = hic.getChromosomes()[1:]
+    start_time = time.time()
+
+    if MULTIPROCESSING:
+        process_pool = []
+
+        for idx in range(len(chromosomes)):
+            p = Process(target=process_chromosome, args=(hic, output, resolution, chromosomes[idx], debug ))
+            process_pool.append(p)
+            p.start()
+        
+        for process in process_pool:
+            process.join()
+    else:
+        for idx in range(len(chromosomes)):
+            process_chromosome(hic, output, resolution, chromosomes[idx], True)
+
+
+    end_time = time.time()
+    
+    print('Parsing took {} seconds!'.format(end_time - start_time))
+
+
+
+
+
+def download_all_hic_datasets():
+    for hic_file_key in hic_file_paths.keys():
+        if not os.path.exists(hic_file_paths[hic_file_key]['local_path']):
+            download_file(hic_file_paths[hic_file_key])
+        
+        parse_hic_file(
+            hic_file_paths[hic_file_key]['local_path'], 
+            os.path.join(PARSED_HIC_FILES_DIRECTORY, hic_file_key),
+            hic_data_resolution
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
