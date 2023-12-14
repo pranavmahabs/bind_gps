df16056738b2be0ed1649edcfa65ffefc1affce5
pranavmahabs <90987389+pranavmahabs@users.noreply.github.com>
2023-12-14 11:22:28 -0500

    Completed Module 2

--- module2_cnn/model.py
+++ module2_cnn/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 
 import numpy as np
 
@@ -36,18 +37,47 @@ class GPS_CNN(nn.Module):
 class GPS_MLP(nn.Module):
     def __init__(self, num_channels=24):
         super().__init__()
-        self.fc1 = nn.Linear(in_features=num_channels, out_features=16)
-        self.fc2 = nn.Linear(in_features=16, out_features=2)
+        self.fc1 = nn.Linear(in_features=num_channels, out_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=16)
+        self.fc3 = nn.Linear(in_features=16, out_features=2)
+        self.dropout = nn.Dropout(0.15)
 
     def forward(self, x):
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
         # apply linear layers and relu activations
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
         return x
 
+class GPS_MLP_Dataset(Dataset):
+    def __init__(self, X: str, y: str, device=None):
+        X = np.load(X)
+        y = np.load(y)
+        self.positive_indices = [i for i, x in enumerate(X) if y[i] == 1]
+        self.negative_indices = [i for i, x in enumerate(X) if y[i] == 0]
+        self.num_positive = len(self.positive_indices)
+        self.num_negative = len(self.negative_indices)
+        self.X = torch.tensor(X, dtype=torch.float32, device=device)
+        self.y = torch.tensor(y, dtype=torch.long, device=device)
 
-class GPS_CNN_Dataset(torch.utils.data.Dataset):
+        print("X Shape: ", self.X.shape)
+        print("y Shape: ", self.y.shape)
+
+    def __len__(self):
+        return len(self.X)
+    
+    def get_quantities(self):
+        print("Num Positive: {}, Num Negative {}".format(self.num_positive, self.num_negative))
+
+    def __getitem__(self, idx):
+        X = self.X[idx]
+        y = self.y[idx]
+        return X, y
+
+class GPS_CNN_Dataset(Dataset):
     def __init__(self, X: str, y: str, device=None):
         # read in numpy file for data
         X = np.load(X)
